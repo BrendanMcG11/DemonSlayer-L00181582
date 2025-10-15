23669fdfb2de540a125ecad979db3ab5c4940125
BrendanMcG04 <brendanmcgettigan04@gmail.com>
2025-10-15 13:26:36 +0100

    Added Project

---
+++ DemonSlayer.py
@@ -0,0 +1,25 @@
+# DemonSlayer.py
+
+def add(a, b):
+    """
+    Function to add two numbers.
+    Args:
+    - a (int or float): The first number.
+    - b (int or float): The second number.
+
+    Returns:
+    - sum (int or float): The sum of a and b.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Function to subtract the second number from the first number.
+    Args:
+    - a (int or float): The first number.
+    - b (int or float): The second number to subtract from the first.
+
+    Returns:
+    - result (int or float): The result of a - b.
+    """
+    return a - b
